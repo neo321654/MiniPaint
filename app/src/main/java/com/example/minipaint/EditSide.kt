a50d9bd5282d13f05a0e32c5c0db7bb1449d0f33
neo321654 <neo321654@rambler.ru>
2021-01-23 15:39:28 +0300

    показал клавиатуру, надо сделать дальнейшую обработку

--- app/src/main/java/com/example/minipaint/EditSide.kt
+++ app/src/main/java/com/example/minipaint/EditSide.kt
@@ -1,11 +1,19 @@
 package com.example.minipaint
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class EditSide : AppCompatActivity() {
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.EditText
+
+
+class EditSide : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_side)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 }
